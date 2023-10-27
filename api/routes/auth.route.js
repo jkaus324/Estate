93208893594e8d928be2d64caf29a77e5fea4f95
jkaus324 <92943010+jkaus324@users.noreply.github.com>
2023-10-27 23:02:59 +0530

    Initial commit

---
+++ api/routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import {google, signup,singin} from '../controllers/auth.controller.js'
+
+const router = express.Router();
+
+router.post("/signup",signup);
+router.post("/signin",singin);
+router.post("/google",google);
+
+export default router;
